804d64ac139e7cd653d38165c1d091e3b2cefada
Himank Jain <55919214+Himank-J@users.noreply.github.com>
2025-12-09 00:04:33 +0530

    Added eureka server and client

---
+++ api-gateway-eureka/src/main/java/com/java/microservices/api_gateway_eureka/ApiGatewayEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.java.microservices.api_gateway_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayEurekaApplication.class, args);
+	}
+
+}
